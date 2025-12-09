a5e53cf8a05823947d2afc6d4f52e466711fcc5d
Nathan Choi <nath49ers@gmail.com>
2025-12-09 11:40:17 -0800

    better print

--- lib/commands/print_nl2sql_result.py
+++ lib/commands/print_nl2sql_result.py
@@ -1,25 +1,10 @@
 import csv
 
-# from tabulate import tabulate
-from textwrap import wrap
 
-CELL_WIDTH = 25
-
-
-def wrap_text(text):
-    return "\n".join(wrap(text, width=CELL_WIDTH))
-
-
-def wrap_items(row):
-    return tuple(wrap_text(item) for item in row)
+def print_row(row):
+    print(",".join(row))
 
 
 def run():
     with open("q9.csv", "r") as file:
-        reader = csv.reader(file)
-        rows = list(map(str, reader))
-        header, rows = rows[0], rows[1:]
-
-    print(header)
-    print("\n".join(rows))
-    # print(tabulate(rows, headers=header, tablefmt="grid"))
+        print(file.read())
